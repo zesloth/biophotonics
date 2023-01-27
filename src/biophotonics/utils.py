0270115c354148721d1753b78a6be6f96c603b00
psavakis <pesavakis@gmail.com>
2023-01-27 12:12:32 +0100

    added dropdown menu

--- src/biophotonics/utils.py
+++ src/biophotonics/utils.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel, QLineEdit
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel, QComboBox
 
 
 class TextBoxWidget(QWidget):
@@ -17,3 +18,33 @@ class TextBoxWidget(QWidget):
 
     def get_text(self):
         return self.root_textbox.text()
+
+
+class DropdownMenu(QWidget):
+    def __init__(self, menu_name, *args):
+        super().__init__()
+
+        self.setLayout(QHBoxLayout())
+
+        self.root_label = QLabel(f'{menu_name}: ')
+
+        self.content = []
+        self.root_dropdown = QComboBox(self)
+        for a in args:
+            self.add(a)
+
+        self.layout().addWidget(self.root_label)
+        self.layout().addWidget(self.root_dropdown)
+
+    def add(self, a):
+        b = str(a)
+        if not b in self.content:
+            self.root_dropdown.addItem(b)
+            self.content.append(b)
+
+    def clear(self):
+        self.content = []
+        self.root_dropdown.clear()
+
+    def get_text(self):
+        return self.root_dropdown.currentText()

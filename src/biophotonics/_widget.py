0b4be94dfc658e474b899c84b2f75543d00703db
psavakis <pesavakis@gmail.com>
2023-01-27 11:16:48 +0100

    tidied _widget.py

--- src/biophotonics/_widget.py
+++ src/biophotonics/_widget.py
@@ -1,10 +1,13 @@
 """
 This module is an example of a barebones QWidget plugin for napari
-
 It implements the Widget specification.
 see: https://napari.org/stable/plugins/guides.html?#widgets
 
-Replace code below according to your needs.
+This file provides an interface for functionality that will be used in the practical.
+Look into the respective .py files for hte actual code.
+
+Code is organised as three separate widgets, because putting all functions into one large
+widget runs into problems on small displays.
 """
 from typing import TYPE_CHECKING
 
@@ -12,25 +15,37 @@ from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget, QVBoxLayout
 
 from biophotonics.segmentation import RFWidget
 from biophotonics.watershed import WatershedWidget
+from biophotonics.spot_finding import SpotFinderWidget
 
 if TYPE_CHECKING:
     import napari
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
+class RandomForest(QWidget):
     def __init__(self, napari_viewer):
         super().__init__()
         self.viewer = napari_viewer
 
         self.rf_widget = RFWidget(self.viewer)
-        self.ws_widget = WatershedWidget(self)
         self.setLayout(QVBoxLayout())
         self.layout().addWidget(self.rf_widget)
+
+
+class Watershed(QWidget):
+    def __init__(self, napari_viewer):
+        super().__init__()
+        self.viewer = napari_viewer
+
+        self.ws_widget = WatershedWidget(self)
+        self.setLayout(QVBoxLayout())
         self.layout().addWidget(self.ws_widget)
 
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+
+class SpotFinder(QWidget):
+    def __init__(self, napari_viewer):
+        super().__init__()
+        self.viewer = napari_viewer
+
+        self.sf_widget = SpotFinderWidget(self.viewer)
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self.sf_widget)

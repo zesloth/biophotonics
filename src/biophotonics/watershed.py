01a2bd9eeaf9b0da143c429dc1a9a3e5237f3f13
psavakis <pesavakis@gmail.com>
2023-01-27 18:39:53 +0100

    added save functions, fixed bugs

--- src/biophotonics/watershed.py
+++ src/biophotonics/watershed.py
@@ -1,7 +1,10 @@
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QLabel
 import numpy as np
+import pandas as pd
 from biophotonics.utils import TextBoxWidget
 import napari
+
+
 class WatershedWidget(QWidget):
     def __init__(self, parent):
         super().__init__()
@@ -9,19 +12,21 @@ class WatershedWidget(QWidget):
         self.parent = parent
         self.viewer = parent.viewer
 
-        #widgets
+        # widgets
         self.title = QLabel()
         self.title.setText('biophotonics')
 
-        self.seed_intensity_threshold = TextBoxWidget('minimum seed intensity', '15000')
-        self.min_seed_distance = TextBoxWidget('minimum seed distance', '20')
+        self.seed_intensity_threshold = TextBoxWidget('minimum nucleus intensity', '15000')
+        self.min_seed_distance = TextBoxWidget('minimum nucleus distance', '20')
 
-        self.generate_seeds_button = QPushButton("generate seeds")
+        self.generate_seeds_button = QPushButton("find nuclei")
         self.generate_seeds_button.clicked.connect(self.generate_seeds)
         self.watershed_button = QPushButton("watershed")
         self.watershed_button.clicked.connect(self.watershed)
 
-        # attributes
+        self.watershed_save_path = TextBoxWidget('segmentation save path', 'segmentation.tif')
+        self.watershed_save_button = QPushButton("save segmentation")
+        self.watershed_save_button.clicked.connect(self.save_watershed)
 
         # layout
         self.setLayout(QVBoxLayout())
@@ -30,7 +35,8 @@ class WatershedWidget(QWidget):
         self.layout().addWidget(self.min_seed_distance)
         self.layout().addWidget(self.generate_seeds_button)
         self.layout().addWidget(self.watershed_button)
-
+        self.layout().addWidget(self.watershed_save_path)
+        self.layout().addWidget(self.watershed_save_button)
 
     def generate_seeds(self):
         from skimage.feature import peak_local_max
@@ -39,17 +45,13 @@ class WatershedWidget(QWidget):
         img = self.viewer.layers.selection.active.data
         img = np.max(img[15:25, ...], axis=0)
 
-
-        name = self.viewer.layers.selection.active.name
-
         thresh = int(self.seed_intensity_threshold.get_text())
         dist = int(self.min_seed_distance.get_text())
 
-        img_mask = img>thresh
+        img_mask = img > thresh
         coords = peak_local_max(gaussian(img_mask, 1), min_distance=dist)
 
-        self.viewer.add_points(coords, name=f'seeds')
-
+        self.viewer.add_points(coords, name=f'nuclei')
 
     def watershed(self):
         import scipy.ndimage as ndi
@@ -58,25 +60,34 @@ class WatershedWidget(QWidget):
         img = self.viewer.layers.selection.active.data
         print(self.viewer.layers.selection.active)
 
-        seeds = self.viewer.layers['seeds'].data
+        seeds = self.viewer.layers['nuclei'].data
 
         # ensure 2d seeds for the moment
         if seeds.shape[-1] > 2:
-            seeds = seeds[:,1:]
+            seeds = seeds[:, 1:]
 
-        landscape = img[0,...] - 2*img[1,...] - img[2,...]
+        landscape = img[0, ...] - 2 * img[1, ...] - img[2, ...]
 
         borders = landscape < 1
 
-        landscape = gaussian(1.*landscape, 2)
-        self.viewer.add_image(landscape, name='gaussian')
+        landscape = gaussian(1. * landscape, 2)
 
         seed_map = np.zeros_like(landscape, dtype=bool)
         seed_map[tuple(seeds.T.astype(int))] = 1
         seed_map, _ = ndi.label(seed_map)
 
         segmentation = watershed(landscape, seed_map, mask=borders)
-        # # dummy for multi-image datasets:
-        segmentation = segmentation[np.newaxis, ...]
-        segmentation = np.concatenate(3*[segmentation], axis=0)
         self.viewer.add_labels(segmentation, name='segmentation')
+
+    def save_watershed(self):
+        from skimage import io
+        if not 'segmentation' in self.viewer.layers:
+            raise ReferenceError('please ensure there is a layer named "segmentation"')
+        save_path = self.watershed_save_path.get_text()
+        img = self.viewer.layers['segmentation'].data
+        io.imsave(save_path, img)
+
+        labels = set(img.ravel()) - {0}
+
+        pd.DataFrame({'cell_label':list(labels)}).to_pickle(f'{save_path.split(".")[0]}.pkl')
+
